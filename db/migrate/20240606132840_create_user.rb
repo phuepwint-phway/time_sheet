a2ffa9e9c41f0010cb811cf6679728043777ccbf
phuepwint-phway <phuepwintphway@gmail.com>
2024-06-06 23:21:48 +0800

    migrate Users table & Clock Lists table

---
+++ db/migrate/20240606132840_create_user.rb
@@ -0,0 +1,17 @@
+class CreateUser < ActiveRecord::Migration[7.1]
+  def up
+    create_table :users do |t|
+      t.string  :user_name
+      t.string  :user_email
+      t.string  :password
+      t.integer :role
+      t.date    :created_at
+      t.date    :updated_at
+      t.date    :deleted_at
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
---
+++ db/migrate/20240606132906_create_clock_list.rb
@@ -0,0 +1,19 @@
+class CreateClockList < ActiveRecord::Migration[7.1]
+  def up
+    create_table :clock_lists do |t|
+      t.integer :user_id
+      t.datetime :entry_date
+      t.datetime :clock_in
+      t.datetime :clock_out
+      t.boolean :clocking_in
+      t.text :note
+      t.date :created_at
+      t.date :updated_at
+      t.date :deleted_at
+    end
+  end
+
+  def down
+    drop_table :clock_lists
+  end
+end
